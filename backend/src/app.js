9faad607ef9300669ddda794d2c715dcbd9f3e80
balajiNadar08 <balajigittttt@gmail.com>
2026-01-08 00:26:28 +0530

    Using route with express

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import authRouter from "./routes/auth.route";
+import userRouter from "./routes/user.route";
+import moviesRouter from "./routes/movies.route";
 
 const app = express();
 app.use(express.json());
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
+app.use("/movies", moviesRouter)
 
 app.get("/", (req, res) => {
   res.send("API is running...");
