ff8fb101c8ab76e4e7ceb2489794a895a9a43122
balajiNadar08 <balajigittttt@gmail.com>
2026-01-13 23:07:47 +0530

    Controller to edit user profile details

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -17,12 +17,59 @@ export const getMyProfile = async (req, res) => {
       success: true,
       data: user,
     });
+
   } catch (error) {
     console.error("Error fetching profile:", error);
 
     res.status(500).json({
       success: false,
       message: "Failed to fetch profile",
+    });
+  }
+};
+
+
+//* PUT: /user/me
+export const editMyProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { name, avatar, theme } = req.body;
+
+    const updateData = {};
+    if (name !== undefined) updateData.name = name;
+    if (avatar !== undefined) updateData.avatar = avatar;
+    if (theme !== undefined) updateData.theme = theme;
+
+    if (Object.keys(updateData).length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "No valid fields provided for update",
+      });
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: updatedUser
+    });
+
+  } catch (error) {
+    console.error("Failed to edit profile:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to edit user profile",
     });
   }
 };
@@ -47,6 +94,7 @@ export const deleteMyProfile = async (req, res) => {
       success: true,
       message: "User deleted successfully",
     });
+
   } catch (error) {
     console.error("Error deleting profile:", error);
     res.status(500).json({
