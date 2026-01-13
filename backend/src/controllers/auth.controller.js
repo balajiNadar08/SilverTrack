b99075afc31092b2df12f7cfb5de9f268a0de2e6
balajiNadar08 <balajigittttt@gmail.com>
2026-01-13 23:27:59 +0530

    Made auth controller to register user

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,57 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+//* POST: /auth/registerUser 
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, avatar } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Name, email and password are required",
+      });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      avatar
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(201).json({
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar
+      }
+    });
+
+  } catch (error) {
+    console.error("Failed to register:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to create user",
+    });
+  }
+};
