728339c4ae712c46af59cc56550517e693b3005e
balajiNadar08 <balajigittttt@gmail.com>
2026-01-20 22:15:15 +0530

    Importing and using error middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import authRouter from "./routes/auth.route";
 import userRouter from "./routes/user.route";
 import moviesRouter from "./routes/movies.route";
+import errorMiddleware from "../middlewares/error.middleware.js";
 
 const app = express();
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/movies", moviesRouter)
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
