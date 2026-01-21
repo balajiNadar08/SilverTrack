425f58b9e4130435802c3d44eb306a05d0e5ef58
balajiNadar08 <balajigittttt@gmail.com>
2026-01-21 22:40:33 +0530

    Using validators for POST requests

--- backend/src/routes/movies.route.js
+++ backend/src/routes/movies.route.js
@@ -7,6 +7,8 @@ import {
   deleteMovie,
   searchMoviesController
 } from "../controllers/movies.controller.js";
+import { createMovieValidator } from "../validators/movie.validator.js";
+import { validate } from "../middlewares/validate.middleware.js";
 
 const moviesRouter = Router();
 
@@ -14,7 +16,7 @@ moviesRouter.get("/", getAllMovies);
 
 moviesRouter.get("/:id", getSpecificMovie);
 
-moviesRouter.post("/", addMovie);
+moviesRouter.post("/", createMovieValidator, validate, addMovie);
 
 moviesRouter.put("/:id", editMovie);
 
