e0ee70119bc51ea3c36d68704605f2d18fcd57d3
balajiNadar08 <balajigittttt@gmail.com>
2026-02-03 22:18:06 +0530

    Added a navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { Zalando_Sans_Expanded } from "next/font/google";
-import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
+import Navbar from "@/components/Navbar";
 
 const zalando = Zalando_Sans_Expanded({
   subsets: ["latin"],
@@ -12,25 +9,8 @@ const zalando = Zalando_Sans_Expanded({
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1 className={`text-4xl font-bold ${zalando.className}`}>SilverTrack</h1>
-
-      <Field className={`w-3xl mt-12 ${zalando.className}`}>
-        <FieldLabel htmlFor="input-field-username">Search</FieldLabel>
-        <div className="flex">
-          <Input
-            id="input-field-username"
-            type="text"
-            placeholder="Search any movie..."
-            className="border-0"
-          />
-          <Button className="cursor-pointer" variant="outline" size="icon">
-            <Search />
-          </Button>
-        </div>
-
-        <FieldDescription>...</FieldDescription>
-      </Field>
+    <div className="bg-[#F8F9FA]">
+      <Navbar />
     </div>
   );
 }
