16b4d9ce2fbd10cf2e079592c5ec9c45734808c9
balajiNadar08 <balajigittttt@gmail.com>
2026-02-15 01:46:46 +0530

    Using CORS in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,27 @@
 import express from "express";
+import cors from "cors";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import moviesRouter from "./routes/movies.route.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+}));
+
 app.use(express.json());
+
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
-app.use("/movies", moviesRouter)
-
-app.use(errorMiddleware);
+app.use("/movies", moviesRouter);
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use(errorMiddleware);
 
 export default app;
