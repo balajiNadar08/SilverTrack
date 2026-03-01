dc0072e9d5fac7e60312aadb0e2daf4248049f33
balajiNadar08 <balajigittttt@gmail.com>
2026-03-02 00:49:39 +0530

    Changed the error message

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -23,7 +23,7 @@ const authMiddleware = (req, res, next) => {
   } catch (error) {
     return res.status(401).json({
       success: false,
-      message: "Invalid or expired token",
+      message: "Login to add movie in SILVERARCHIVE",
     });
   }
 };
