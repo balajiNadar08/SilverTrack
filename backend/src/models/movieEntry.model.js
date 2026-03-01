15441d387d530f503c35302517ee94e2ddce1a57
balajiNadar08 <balajigittttt@gmail.com>
2026-03-02 00:49:39 +0530

    Added new required values

--- backend/src/models/movieEntry.model.js
+++ backend/src/models/movieEntry.model.js
@@ -1,37 +1,72 @@
 import mongoose from "mongoose";
 
-const movieEntrySchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    index: true
+const movieEntrySchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    tmdbMovieId: {
+      type: Number,
+      required: true,
+    },
+
+    original_title: {
+      type: String,
+      required: true,
+    },
+
+    backdrop_path: {
+      type: String,
+    },
+
+    poster_path: {
+      type: String,
+    },
+
+    overview: {
+      type: String,
+    },
+
+    original_language: {
+      type: String,
+    },
+
+    release_date: {
+      type: String,
+    },
+
+    status: {
+      type: String,
+      enum: ["planned", "watching", "completed"],
+      default: "completed",
+    },
+
+    rating: {
+      type: Number,
+      required: [true, "Rating is required"],
+      min: 0,
+      max: 10,
+    },
+
+    note: {
+      type: String,
+      trim: true,
+      maxLength: 256,
+    },
+
+    watchedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  tmdbMovieId: {
-    type: Number,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ["planned", "watching", "completed"],
-    default: "completed"
-  },
-  rating: {
-    type: Number,
-    required: [true, "Rating is required"],
-    min: 0,
-    max: 10
-  },
-  note: {
-    type: String,
-    trim: true,
-    maxLength: 256, 
-  },
-  watchedAt: {
-    type: Date,
-    default: Date.now
-  }
-}, { timestamp: true });
+  { timestamps: true },
+);
+
+movieEntrySchema.index({ userId: 1, tmdbMovieId: 1 }, { unique: true });
 
 const MovieEntry = mongoose.model("MovieEntry", movieEntrySchema);
 
