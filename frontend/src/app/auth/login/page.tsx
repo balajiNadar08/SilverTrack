dcc19bef51644a03f23f8a1a4b474c3b5971ae0b
balajiNadar08 <balajigittttt@gmail.com>
2026-03-04 21:58:38 +0530

    Added backend API URL

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -31,7 +31,7 @@ const Page = () => {
     try {
       setLoading(true);
 
-      const response = await fetch("http://localhost:8000/auth/login", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
