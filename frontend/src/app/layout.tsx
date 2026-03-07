4974e73af2f316ed860de0dcca8225716cc3f52c
balajiNadar08 <balajigittttt@gmail.com>
2026-03-07 21:00:41 +0530

    Minor changes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <CustomCursor />
       <body className="min-h-screen flex flex-col">
+        <CustomCursor />
         <Navbar />
         <main className="flex-1">
           {children}
@@ -27,4 +27,3 @@ export default function RootLayout({
     </html>
   );
 }
-
